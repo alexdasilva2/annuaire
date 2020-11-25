953630aaf3d1479ed307a8cd8664237ec209fedb
Floriane <floriane.ahadi@gmail.com>
2020-11-25 09:42:33 +0100

    ds model, création classe Stagiaire

--- Groupe1/src/fr/eql/ai108/annuaire/model/TestGit.java
+++ Groupe1/src/fr/eql/ai108/annuaire/model/TestGit.java
@@ -3,8 +3,8 @@ package fr.eql.ai108.annuaire.model;
 public class TestGit {
 
 	public static void main(String[] args) {
-System.out.println("coucou");
-System.out.println("re coucou");
+		System.out.println("coucou");
+		System.out.println("re coucou");
 	}
 
 }
