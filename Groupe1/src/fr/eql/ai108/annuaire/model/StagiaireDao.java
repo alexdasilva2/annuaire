f35a7b6bcb7ee912fbeaff0a031e71aee3937333
Floriane <floriane.ahadi@gmail.com>
2020-11-25 09:59:14 +0100

    ds model, creation StagiaireDao

---
+++ Groupe1/src/fr/eql/ai108/annuaire/model/StagiaireDao.java
@@ -0,0 +1,10 @@
+package fr.eql.ai108.annuaire.model;
+
+import java.io.File;
+
+public class StagiaireDao {
+	
+	private File file = new File("stagiaire.bin");
+	
+
+}
--- Groupe1/src/fr/eql/ai108/annuaire/model/TestGit.java
+++ Groupe1/src/fr/eql/ai108/annuaire/model/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("coucou");
 		System.out.println("re coucou");
+		System.out.println("re re coucou");
 	}
 
 }
