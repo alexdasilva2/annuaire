1ef2cf31b4a9c532a777f5e2b3bbc6903c2210fb
Floriane <floriane.ahadi@gmail.com>
2020-11-26 08:56:20 +0100

    ajout méthode "compterChar" dans StagiaireDao

--- Groupe1/src/fr/eql/ai108/annuaire/model/StagiaireDao.java
+++ Groupe1/src/fr/eql/ai108/annuaire/model/StagiaireDao.java
@@ -1,16 +1,18 @@
 package fr.eql.ai108.annuaire.model;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StagiaireDao {
-	
+
 	private File file = new File("stagiaire.bin");
-	
-	
+
+
 	//methode getAll : on doit avoir la structure de notre fichier binaire 
 	//avant de pouvoir l'implémenter
 	public List<Stagiaire> getAll() {
@@ -24,9 +26,98 @@ public class StagiaireDao {
 		}
 		return null;
 	}
-	
-	
+
+
 	public void nouvelleMethode() {
 		//pour la mise en forme : deux espaces entre chaque méthode
 	}
-}
+
+
+	public static int[] compterChar(BufferedReader inputFile) {
+		int[] taillesMax = {0,0,0,0,0};
+		int tailleChamp = 0;
+		int nbSautDeLigne =0;
+		String ligne = "";
+
+		try {
+			ligne = inputFile.readLine();
+			while (ligne != null) {
+				if (ligne.compareTo("*") ==0) {
+					nbSautDeLigne=0;
+				} else {
+					char[] tableau = ligne.toCharArray();
+					switch (nbSautDeLigne) {
+					case 0 :
+						for (int i = 0; i< tableau.length; i++) {
+							tailleChamp++;
+							if (tailleChamp > taillesMax[0]) {
+								taillesMax[0] = tailleChamp;
+							}
+						}
+						tailleChamp=0;
+						nbSautDeLigne++;
+						break;
+					case 1:
+						for (int i = 0; i< tableau.length; i++) {
+							tailleChamp++;
+							if (tailleChamp > taillesMax[1]) {
+								taillesMax[1] = tailleChamp;
+							}
+						}
+						tailleChamp=0;
+						nbSautDeLigne++;
+						break;
+					case 2:
+						for (int i = 0; i< tableau.length; i++) {
+							tailleChamp++;
+							if (tailleChamp > taillesMax[2]) {
+								taillesMax[2] = tailleChamp;
+							}
+						}
+						tailleChamp=0;
+						nbSautDeLigne++;
+						break;
+					case 3:
+						for (int i = 0; i< tableau.length; i++) {
+							tailleChamp++;
+							if (tailleChamp > taillesMax[3]) {
+								taillesMax[3] = tailleChamp;
+							}
+						}
+						tailleChamp=0;
+						nbSautDeLigne++;
+						break;
+					case 4:
+						for (int i = 0; i< tableau.length; i++) {
+							tailleChamp++;
+							if (tailleChamp > taillesMax[4]) {
+								taillesMax[4] = tailleChamp;
+							}
+						}
+						tailleChamp=0;
+						nbSautDeLigne++;
+						break;
+					default:
+						break;
+					}
+				}
+				ligne = inputFile.readLine();
+			}
+
+		} catch (IOException e) {
+			System.out.println("IO exception");
+			e.printStackTrace();
+		} finally {
+			try {
+				inputFile.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return taillesMax;
+	}
+
+
+
+	}
